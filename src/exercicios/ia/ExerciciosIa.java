cd6a583d2e7656def846445c2df8f2bf085f151f
Fabio Augusto <30555110+AugustoFabio4@users.noreply.github.com>
2020-09-28 21:43:51 -0300

    Atualização do codigo para atividade 6

--- src/exercicios/ia/ExerciciosIa.java
+++ src/exercicios/ia/ExerciciosIa.java
@@ -44,7 +44,7 @@ public class ExerciciosIa {
         if (atividade == 6) {
         System.out.println("Faça um programa que leia 10 tarefas. Cada tarefa contém uma descrição (string) e uma prioridade (inteiro de 0 a 5. Utilizar classe ou estrutura para representar a tarefa). As tarefas devem ser inseridas em uma fila de prioridade. programa deve imprimir as tarefas na tela de acordo com a sua prioridade.\n");
         atividade6 atv6 = new atividade6();
-        //atv6.minhafila();
+        atv6.tarefasporprioridade();
         System.out.println("-----------------------------");
         }
         }
